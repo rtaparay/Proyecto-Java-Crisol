355cee773ee43529dd4ac38da9e03048dc2c830e
AlbertoPado210 <138325458+AlbertoPado210@users.noreply.github.com>
2023-07-04 20:22:20 -0500

    Update Categoria.java

--- TrabajoFinal/Categoria.java
+++ TrabajoFinal/Categoria.java
@@ -1,4 +1,5 @@
 package TrabajoFinal;
+import java.util.Scanner;
 
 public class Categoria {
     private int id_categoria;
@@ -43,18 +44,57 @@ public class Categoria {
     }
 
     public void registrar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el ID de la categoría: ");
+        id_categoria = scanner.nextInt();
+
+        System.out.println("Ingrese el nombre de la categoría: ");
+        scanner.nextLine();
+        nombreCategoria = scanner.nextLine();
+
+        System.out.println("Ingrese la información de libros relacionados: ");
+        libros = scanner.nextLine();
+
+        System.out.println("Ingrese la información de productos relacionados: ");
+        productos = scanner.nextLine();
+
+        System.out.println("La categoría ha sido registrada con éxito.");
 
     }
 
     public void modificar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el nuevo nombre de la categoría: ");
+        nombreCategoria = scanner.nextLine();
+
+        System.out.println("Ingrese la nueva información de libros relacionados: ");
+        libros = scanner.nextLine();
+
+        System.out.println("Ingrese la nueva información de productos relacionados: ");
+        productos = scanner.nextLine();
+
+        System.out.println("La categoría ha sido modificada con éxito.");
 
     }
 
     public void eliminar() {
+        id_categoria = 0;
+        nombreCategoria = "";
+        libros = "";
+        productos = "";
+
+        System.out.println("La categoría ha sido eliminada.");
 
     }
 
     public void imprimir() {
+        System.out.println("Detalles de la categoría:");
+        System.out.println("ID: " + id_categoria);
+        System.out.println("Nombre: " + nombreCategoria);
+        System.out.println("Libros: " + libros);
+        System.out.println("Productos: " + productos);
 
     }
 }
