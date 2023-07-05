fc558c033e50c832609bd2324b531a4e81e55068
AlbertoPado210 <138325458+AlbertoPado210@users.noreply.github.com>
2023-07-04 20:31:46 -0500

    Update Producto.java

--- TrabajoFinal/Producto.java
+++ TrabajoFinal/Producto.java
@@ -1,4 +1,5 @@
 package TrabajoFinal;
+import java.util.Scanner;
 
 public class Producto {
     private int id_producto;
@@ -36,19 +37,46 @@ public class Producto {
     }
 
     public void registrar() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese el ID del producto: ");
+        id_producto = scanner.nextInt();
+
+        System.out.println("Ingrese el precio del producto: ");
+        precio = scanner.nextDouble();
+
+        System.out.println("Ingrese el tipo de producto: ");
+        scanner.nextLine(); // Limpiar el buffer de entrada
+        tipoProducto = scanner.nextLine();
+
+        System.out.println("El producto ha sido registrado con éxito.");
     }
 
     public void modificar() {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Ingrese el nuevo precio del producto: ");
+        precio = scanner.nextDouble();
+
+        System.out.println("Ingrese el nuevo tipo de producto: ");
+        scanner.nextLine(); // Limpiar el buffer de entrada
+        tipoProducto = scanner.nextLine();
+
+        System.out.println("El producto ha sido modificado con éxito.");
     }
 
     public void eliminar() {
+        id_producto = 0;
+        precio = 0.0;
+        tipoProducto = "";
 
+        System.out.println("El producto ha sido eliminado.");
     }
 
     public void imprimir() {
-
-
+        System.out.println("Detalles del producto:");
+        System.out.println("ID: " + id_producto);
+        System.out.println("Precio: " + precio);
+        System.out.println("Tipo de producto: " + tipoProducto);
     }
 }
