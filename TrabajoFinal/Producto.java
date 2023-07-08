23fc1c01ba5e0514d87d4eceebaa204816db6963
AlbertoPado210 <138325458+AlbertoPado210@users.noreply.github.com>
2023-07-07 21:25:10 -0500

    Update Producto.java

--- TrabajoFinal/Producto.java
+++ TrabajoFinal/Producto.java
@@ -36,47 +36,12 @@ public class Producto {
         this.tipoProducto = tipoProducto;
     }
 
-    public void registrar() {
-        Scanner scanner = new Scanner(System.in);
+       public abstract void registrar(Libro libro, ArrayList<Libro> listaLibros);
 
-        System.out.println("Ingrese el ID del producto: ");
-        id_producto = scanner.nextInt();
+    public abstract void modificar(int idLibro, String nuevoISBN, String nuevoNombreTitulo, int nuevoIdProveedor, String nuevaEdicion,
+                                   String nuevoAutor, int nuevoIdCategoria, ArrayList<Libro> listaLibros);
 
-        System.out.println("Ingrese el precio del producto: ");
-        precio = scanner.nextDouble();
+    public abstract void eliminar(int idLibro, ArrayList<Libro> listaLibros);
 
-        System.out.println("Ingrese el tipo de producto: ");
-        scanner.nextLine(); // Limpiar el buffer de entrada
-        tipoProducto = scanner.nextLine();
-
-        System.out.println("El producto ha sido registrado con éxito.");
-    }
-
-    public void modificar() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ingrese el nuevo precio del producto: ");
-        precio = scanner.nextDouble();
-
-        System.out.println("Ingrese el nuevo tipo de producto: ");
-        scanner.nextLine(); // Limpiar el buffer de entrada
-        tipoProducto = scanner.nextLine();
-
-        System.out.println("El producto ha sido modificado con éxito.");
-    }
-
-    public void eliminar() {
-        id_producto = 0;
-        precio = 0.0;
-        tipoProducto = "";
-
-        System.out.println("El producto ha sido eliminado.");
-    }
-
-    public void imprimir() {
-        System.out.println("Detalles del producto:");
-        System.out.println("ID: " + id_producto);
-        System.out.println("Precio: " + precio);
-        System.out.println("Tipo de producto: " + tipoProducto);
-    }
+    public abstract void imprimir();
 }
